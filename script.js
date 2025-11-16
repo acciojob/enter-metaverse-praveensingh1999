692221e9bd30ff175092c8a3620d126cf5f48334
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-11-16 12:19:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const status = document.querySelector("#status");
+const heading = document.querySelector("#heading");
+function changetext(){
+	status.style.display="none";
+	heading.style.display="block";
+}
